cb28a29d38a24e902a51b8b623b28a3659e67cfe
catvonnegut <mulvaneytara@gmail.com>
2019-10-29 23:37:26 -0400

    editing the path to the response file that returns if environment = prod

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use("/", express.static("./build/"));
 
 if (process.env.NODE_ENV == "production") {
   app.get('/',function(req,res){
-      res.sendFile(__dirname + '/index.html');
+      res.sendFile(path.join(__dirname + '/index.html'));//need to find the correct response file
   });
 }
 
