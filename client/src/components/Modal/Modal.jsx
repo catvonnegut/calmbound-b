da979ae5386cc97167c047e6ffc28281b2b9a571
catvonnegut <mulvaneytara@gmail.com>
2019-12-14 02:35:27 -0500

    initial modal design work, still need to pass props to the modal

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import './Modal.css';
+import add from '../../assets/addToCartButton.png';
+import buy from '../../assets/buyWholesaleButton.png';
+import add1 from '../../assets/add1.png';
+import remove1 from '../../assets/remove1.png';
 
 function Modal(props) {
   return (
-    <div className="modal_style">
+    <div className="modal_style"
+    onClick={props.modalClickHandler}>
       <header className="modal_header">
-      <div className="image_placeholder"></div>
-      {props.title}
+      <div className="image_placeholder">{props.name}</div>
+
       <> </>
       {props.price}
       </header>
       <section className="modal_content">
         <label>
-          <input type="text" name="count" placeholder="1"></input>
-          <button type="click" name="delete" placeholder="-">-</button>
-          <button type="click" name="delete" placeholder="+">+</button>
+          <input className="counter" type="text" name="count" placeholder="1"></input>
+          <button className="remove" type="click" name="remove">
+            <img src={remove1} alt="Remove 1"/>
+          </button>
+          <button className="add" type="click" name="add">
+            <img src={add1} alt="Add 1"/>
+          </button>
+
         </label>
         {props.description}
 
       </section>
       <section className="modal_actions">
-        <button className="btn">Add to cart</button>
-        <button className="btn">Buy wholesale</button>
+        <button className="addToCart">
+          <img src={add} alt="Add to Cart" />
+        </button>
+        <button className="buyWholesale">
+          <img src={buy} alt="Buy Wholesale" />
+        </button>
       </section>
     </div>
   )
